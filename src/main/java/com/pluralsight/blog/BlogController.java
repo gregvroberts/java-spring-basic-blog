9898eeaf4cafdeaf6fe61a331ffb4a556cac3d3a
Gregory Roberts <roberts.gregory@heb.com>
2021-09-09 13:55:08 -0500

    Display static data from post repo

--- src/main/java/com/pluralsight/blog/BlogController.java
+++ src/main/java/com/pluralsight/blog/BlogController.java
@@ -1,14 +1,27 @@
 package com.pluralsight.blog;
 
+import com.pluralsight.blog.data.PostRepository;
+import com.pluralsight.blog.model.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class BlogController {
+
+    public BlogController(PostRepository pr) {
+        postRepository = pr;
+    }
+
+    private PostRepository postRepository;
+
     @RequestMapping("/")
     public String listPosts(ModelMap modelMap) {
-        modelMap.put("title", "Blog Post 1");
+
+        List<Post> posts = postRepository.getAllPosts();
+        modelMap.put("posts", posts);
         return "home";
     }
 }
